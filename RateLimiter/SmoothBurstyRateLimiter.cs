5ed9eacfc63be43cca5f8fc286ced68dcb866e12
Shuai Zhang <zhangshuai.ustc@gmail.com>
2018-01-02 17:25:23 +0800

    Add SmoothBurstyRateLimiter.

---
+++ RateLimiter/SmoothBurstyRateLimiter.cs
@@ -0,0 +1,39 @@
+using System;
+using Clocks;
+
+namespace RateLimiter
+{
+    public sealed class SmoothBurstyRateLimiter : SmoothRateLimiter
+    {
+        public SmoothBurstyRateLimiter(
+            IStopwatchProvider<long> stopwatchProvider,
+            double maxBurstSeconds)
+            : base(stopwatchProvider)
+        {
+            this.maxBurstSeconds = maxBurstSeconds;
+        }
+
+        private readonly double maxBurstSeconds;
+
+        protected override TimeSpan CoolDownInterval => stableInterval;
+
+        protected override void DoSetRate(double permitsPerSecond, TimeSpan stableInterval)
+        {
+            var oldMaxPermits = maxPermits;
+            maxPermits = maxBurstSeconds * permitsPerSecond;
+            if (double.IsPositiveInfinity(oldMaxPermits))
+            {
+                storedPermits = maxPermits;
+            }
+            else
+            {
+                storedPermits = (oldMaxPermits == 0) ? 0 : storedPermits * maxPermits / oldMaxPermits;
+            }
+        }
+
+        protected override TimeSpan StoredPermitsToWaitTime(double storedPermits, double permitsToTake)
+        {
+            return TimeSpan.Zero;
+        }
+    }
+}
